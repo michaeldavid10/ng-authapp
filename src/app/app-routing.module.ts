c733a8d39b0a1f08fe3d88a474ef105275633d07
Michael David <mdavidmachacuay@gmail.com>
2019-05-21 14:22:34 -0500

    create guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
 import {PricesComponent} from './pages/prices/prices.component';
 import {ProtectecComponent} from './pages/protectec/protectec.component';
+import {AuthGuard} from './guards/auth.guard';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'precios', component: PricesComponent },
-  { path: 'protegida', component: ProtectecComponent },
+  { path: 'protegida', component: ProtectecComponent, canActivate: [AuthGuard] },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
