2b9f9b8deec0d3e29e05ef063634972f4ac8e072
Michael David <mdavidmachacuay@gmail.com>
2019-05-21 13:39:31 -0500

    create auth service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import * as auth0 from 'auth0-js';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private _idToken: string;
+  private _accessToken: string;
+  private _expiresAt: number;
+
+  auth0 = new auth0.WebAuth({
+    clientID: 'nzjsbGSZrZfXuuf5pFF52xuc9ANTq2PO',
+    domain: 'michaelmach10.auth0.com',
+    responseType: 'token id_token',
+    redirectUri: 'http://localhost:4200/callback',
+    scope: 'openid'
+  });
+
+  constructor(public router: Router) {
+    this._idToken = '';
+    this._accessToken = '';
+    this._expiresAt = 0;
+  }
+
+  get accessToken(): string {
+    return this._accessToken;
+  }
+
+  get idToken(): string {
+    return this._idToken;
+  }
+
+  public login(): void {
+    this.auth0.authorize();
+  }
+
+}
